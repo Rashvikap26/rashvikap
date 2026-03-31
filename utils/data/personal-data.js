cc66a4b8df122bf157f4e963c7f472bc707bb71e
Rashvika Ponduri <rashvikaponduri26@gmail.com>
2026-03-31 17:55:49 -0400

    Updated photo

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Rashvika Ponduri",
-  profile: '/photo.jpeg',
+  profile: '/image/photo.jpeg',
   designation: "Java Developer",
   description: "My name is Rashvika Ponduri. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I enjoy working across different layers of application development, with Java being my core strength. I like building scalable and reliable applications using Java and modern full stack technologies. I am open to opportunities that match my skills, experience, and career interests.",
   email: 'rashi.p1930@gmail.com',
